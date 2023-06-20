dee0369b9df599f4e54090b064acead3f554d350
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-06-21 00:50:30 +0530

    Added Styles index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,41 +4,47 @@ import { CssBaseline } from '@mui/material';
 
 import { NavBar, Movies, MovieInfo, Profile, Actors } from '.';
 
-const App = () => (
-  <div>
+import useStyles from './styles';
 
-    <CssBaseline />
-    <NavBar />
-    <main>
-      <Switch>
-        <Route exact path="/movie/:id">
-          <h1>
-            Movie Information
-            <MovieInfo />
-          </h1>
-        </Route>
-        <Route exact path="/actors/:id">
-          <h1>
-            Actors
-            <Actors />
-          </h1>
-        </Route>
-        <Route path="/movies">
-          <h1>
-            Movies Home - Filmpire
-            <Movies />
-          </h1>
-        </Route>
-        <Route path="/profile/:id">
-          <h1>
-            Profile - Filmpire
-            <Profile />
-          </h1>
-        </Route>
+const App = () => {
+  const classes = useStyles();
 
-      </Switch>
-    </main>
-  </div>
-);
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movie/:id">
+            <h1>
+              Movie Information
+              <MovieInfo />
+            </h1>
+          </Route>
+          <Route exact path="/actors/:id">
+            <h1>
+              Actors
+              <Actors />
+            </h1>
+          </Route>
+          <Route path="/">
+            <h1>
+              Movies Home - Filmpire
+              <Movies />
+            </h1>
+          </Route>
+          <Route path="/profile/:id">
+            <h1>
+              Profile - Filmpire
+              <Profile />
+            </h1>
+          </Route>
+
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles(() => ({
+  root: {
+    display: 'flex',
+    height: '100vh',
+  },
+  toobar: {
+    height: '64px',
+  },
+  content: {
+    flexGrow: '1',
+    padding: '2em',
+  },
+}));
