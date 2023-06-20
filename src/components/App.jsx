ede6476d191bf3e51c3317748ed6e93dbbca5353
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-06-21 00:34:36 +0530

    Added more components like MovieInfo, Movies, Profile, NavBar, etc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+
+import { NavBar, Movies, MovieInfo, Profile, Actors } from '.';
 
 const App = () => (
   <div>
-    App
+
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Switch>
+        <Route exact path="/movie/:id">
+          <h1>
+            Movie Information
+            <MovieInfo />
+          </h1>
+        </Route>
+        <Route exact path="/actors/:id">
+          <h1>
+            Actors
+            <Actors />
+          </h1>
+        </Route>
+        <Route path="/movies">
+          <h1>
+            Movies Home - Filmpire
+            <Movies />
+          </h1>
+        </Route>
+        <Route path="/profile/:id">
+          <h1>
+            Profile - Filmpire
+            <Profile />
+          </h1>
+        </Route>
+
+      </Switch>
+    </main>
   </div>
 );
 
---
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieInfo = () => {
+  console.log('MovieInfo');
+  return (
+    <div>
+      MovieInformation
+    </div>
+  );
+};
+
+export default MovieInfo;
