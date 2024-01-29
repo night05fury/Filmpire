018d5f40f73c85dccb3a409f2657abdfe2ada2f3
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2024-01-29 22:59:23 +0530

    Create Pagination.jsx

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Typography, Button } from '@mui/material'
+import {useStyle} from './styles';
+
+
+
+
+const Pagination = () => {
+  return (
+    <div>Pagination</div>
+  )
+}
+
+export default Pagination
